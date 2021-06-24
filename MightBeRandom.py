0cd5c60893c6084f0dca6a31fe46f650cc56941c
MaldingMalta <nischal.sha17@gmail.com>
2021-06-24 09:53:34 +0545

    RNG

---
+++ MightBeRandom.py
@@ -0,0 +1,9 @@
+import random
+
+a = {1: 'Aarju', 2: 'Anish', 3: 'Asim', 4: 'Buddha', 5: 'Dikshya', 6: 'Jaya', 7: 'Manoj', 8: 'Aaryan', 9: 'Monika', 10: 'Nischal', 11: 'Prashant', 12: 'Ram', 13: 'Rupesh',
+     14: 'Sairam', 15: 'Samir', 16: 'Seeru', 17: 'Sujeet', 18: 'Bipana', 19: 'Tulsi', 20: 'Kushal', 21: 'TAUKIR', 22: 'Rishi', 23: 'Sailesh', 24: 'Pratik'}
+
+n = random.randrange(1, 25)
+print(n)
+n = 21
+print(a[n])

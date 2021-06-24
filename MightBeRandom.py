d991a6fc15cd2171fe8e1cf368133db12032529f
MaldingMalta <nischal.sha17@gmail.com>
2021-06-24 09:57:27 +0545

    RNG

--- MightBeRandom.py
+++ MightBeRandom.py
@@ -4,6 +4,6 @@ a = {1: 'Aarju', 2: 'Anish', 3: 'Asim', 4: 'Buddha', 5: 'Dikshya', 6: 'Jaya', 7:
      14: 'Sairam', 15: 'Samir', 16: 'Seeru', 17: 'Sujeet', 18: 'Bipana', 19: 'Tulsi', 20: 'Kushal', 21: 'TAUKIR', 22: 'Rishi', 23: 'Sailesh', 24: 'Pratik'}
 
 n = random.randrange(1, 25)
-print(n)
+print(f'Roll Number : {n}')
 n = 21
-print(a[n])
+print(f'Student Name : {a[n]}')

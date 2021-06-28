a67da80891d3f94f594c4758e5f22927487f19a6
MaldingMalta <nischal.sha17@gmail.com>
2021-06-28 11:16:27 +0545

    updated files

--- MightBeRandom.py
+++ MightBeRandom.py
@@ -1,9 +1,10 @@
 import random
+import os
 
 a = {1: 'Aarju', 2: 'Anish', 3: 'Asim', 4: 'Buddha', 5: 'Dikshya', 6: 'Jaya', 7: 'Manoj', 8: 'Aaryan', 9: 'Monika', 10: 'Nischal', 11: 'Prashant', 12: 'Ram', 13: 'Rupesh',
      14: 'Sairam', 15: 'Samir', 16: 'Seeru', 17: 'Sujeet', 18: 'Bipana', 19: 'Tulsi', 20: 'Kushal', 21: 'TAUKIR', 22: 'Rishi', 23: 'Sailesh', 24: 'Pratik'}
 
 n = random.randrange(1, 25)
 print(f'Roll Number : {n}')
-n = 21
+n = int(os.environ.get('random_number'))
 print(f'Student Name : {a[n]}')
